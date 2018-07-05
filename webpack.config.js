b0c825f09471d93be0e053e1587277e59874bd2c
MicMonen <contact@michelmonen.com>
2018-07-05 17:38:59 +0200

    added helper to include config files in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
-
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
+const assign = require('lodash.assign');
+const fsReaddirRecursive = require('fs-readdir-recursive');
+
+function includeFiles(path) {
+    return fsReaddirRecursive(path)
+        .filter(
+            (file) => file.match(/.*\.coffee$/)
+        )
+        .map((file) => {
+            return {
+				name: path + file.substring(0, file.length - 7),
+				path: path + file,
+            }
+        }).reduce((list, file) => {
+            list[file.name] = file.path;
+            return list;
+        }, {})
+}
 
 module.exports = {
-	entry: slsw.lib.entries,
+	entry: assign(includeFiles('./config/'), slsw.lib.entries),
 	target: 'node',
 	mode: slsw.lib.webpack.isLocal ? 'development' : 'production',
 	externals: [nodeExternals()],
@@ -13,12 +30,6 @@ module.exports = {
 			{
 				loader: "coffee-loader",
                 test: /\.coffee$/,
-				options: {
-                    transpile: {
-                        plugins: ['transform-node-env-inline'],
-                        presets: ['env']
-                    }
-                }
 			}
 		],
 	},
